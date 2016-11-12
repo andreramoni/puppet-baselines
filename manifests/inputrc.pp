eaca55226006532ce81296160466c11537597341
Andre Ramoni <ramoni@Andres-iMac.local>
2016-11-12 21:31:25 -0200

    inicital testing.

--- manifests/inputrc.pp
+++ manifests/inputrc.pp
@@ -1,11 +1,34 @@
-class olx_base::linux::inputrc {
-  require olx_base::linux
+# == Class: inputrc
+#
+# This class manipulates /etc/inputrc.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#
+#
+# === Variables
+#
+# [*sample_variable*]
+#
+#
+# === Examples
+#
+#  class { 'trollme':
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
+#
+# === Authors
+#
+# Andre Ramoni
+#
+class baselines::inputrc {
 
   File_line {
     path  => '/etc/inputrc',
     multiple => true,
   }
-  
+
   file_line { 'inputrc_showall':
     ensure => 'present',
     line   => 'set show-all-if-ambiguous on',
@@ -17,4 +40,3 @@ class olx_base::linux::inputrc {
     match  => '^set completion-ignore-case.*',
   }
 }
-
