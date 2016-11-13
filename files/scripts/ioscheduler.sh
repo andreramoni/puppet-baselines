d41cc7be4a81124db22f839e3d3423c8b53026bc
Andre Ramoni <ramoni@Andres-iMac.local>
2016-11-13 00:23:09 -0200

    Added scripts class.

--- files/scripts/ioscheduler.sh
+++ files/scripts/ioscheduler.sh
@@ -3,33 +3,30 @@ DEVICE=$1
 SCHED=$2
 
 function set_io() {
-DEVICE=$1
-SCHED=$2
-if [ ! -d "/sys/block/$DEVICE" ]; then
-	echo "ERR: /sys/block/$DEVICE does not exists."
-	exit
-fi
-
-if [ "$SCHED" != "" ]; then
-	case $SCHED in
-		cfq|noop|antecipatory|deadline) true ;;
-		*) echo "ERR: Invalid scheduler $SCHED." ; exit  ;;
-	esac
-	# Verify support for scheduler:
-	if [ "`grep $SCHED /sys/block/$DEVICE/queue/scheduler`" == "" ]; then
-		echo "ERR: Does not support scheduler $SCHED."
+	DEVICE=$1
+	SCHED=$2
+	if [ ! -d "/sys/block/$DEVICE" ]; then
+		echo "ERR: /sys/block/$DEVICE does not exists."
 		exit
-	else
-		echo $SCHED > /sys/block/$DEVICE/queue/scheduler
 	fi
 
-else
-	# return current scheduler
-	cat /sys/block/$DEVICE/queue/scheduler | cut -d'[' -f2 | cut -d']' -f1
-fi
+	if [ "$SCHED" != "" ]; then
+		case $SCHED in
+			cfq|noop|antecipatory|deadline) true ;;
+			*) echo "ERR: Invalid scheduler $SCHED." ; exit  ;;
+		esac
+		# Verify support for scheduler:
+		if [ "`grep $SCHED /sys/block/$DEVICE/queue/scheduler`" == "" ]; then
+			echo "ERR: Does not support scheduler $SCHED."
+			exit
+		else
+			echo $SCHED > /sys/block/$DEVICE/queue/scheduler
+		fi
+	else
+		# return current scheduler
+		cat /sys/block/$DEVICE/queue/scheduler | cut -d'[' -f2 | cut -d']' -f1
+	fi
 }
-
-
 
 if [ "$DEVICE" == "" ]; then
 	echo "Usage:"
