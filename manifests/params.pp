493b46c3084c8fd5814373b83a991326adcc8803
Andre Ramoni <ramoni@Andres-iMac.local>
2016-11-12 22:02:34 -0200

    inicital testing.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
 class baselines::params {
 
-  
-  # inputrc::
-  $inputrc_options = [ 
-    'set show-all-if-ambiguous on', 
-    'set completion-ignore-case on', 
+
+  # inputrc:
+  $inputrc_options = [
+    'set show-all-if-ambiguous on',
+    'set completion-ignore-case on',
   ]
 
 
