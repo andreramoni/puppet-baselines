c87458227cb6b65b4abcdc166b1fe70556d56fad
Andre Ramoni <ramoni@Andres-MacBook-Pro.local>
2016-11-17 21:34:56 -0200

    testing

--- manifests/sysctl.pp
+++ manifests/sysctl.pp
@@ -26,7 +26,7 @@ define baselines::sysctl (
   augeas { "sysctl_${title}":
       context => '/files/etc/sysctl.conf',
       changes => $sysctl_options,
-      notify  => Class['sysctl_reload'],
   }
+  notify  => Class['sysctl_reload'],
 
 }
