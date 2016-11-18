78939514cb50792c20612b38d2a717d7780837bb
Andre Ramoni <ramoni@Andres-MacBook-Pro.local>
2016-11-17 22:39:02 -0200

    Include sysctl class in init

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,7 +38,7 @@
 class baselines {
   include baselines::hosts_localhost
   include baselines::inputrc
-  include baselines::sysctl_reload
+  include baselines::sysctl
   include baselines::vmwaretools
   include baselines::ssh_server
   include baselines::custom_profile
