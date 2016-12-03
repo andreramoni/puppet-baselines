10e58f13d8d9235fd5435fab1f94c5dc4e7818d7
Andre Ramoni <andre.ramoni@gmail.com>
2016-12-03 16:44:04 -0200

    .

--- manifests/foreman/proxy/ip_forward.pp
+++ manifests/foreman/proxy/ip_forward.pp
@@ -1,5 +1,5 @@
 class rbaselines::foreman::proxy::ip_forward {
   rbaselines::sysctl { 'ip_forwarding':
-    sysctl_options => ['net.ipv4.ip_forward = 1'],
+    sysctl_options => [ 'net.ipv4.ip_forward = 1',] ,
   }
 }
