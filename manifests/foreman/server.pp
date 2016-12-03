be2fe2ed95c3302776746baccec9311dba648f3e
root <root@foreman.localdomain>
2016-12-03 17:00:06 +0000

    Foreman column view plugin.

--- manifests/foreman/server.pp
+++ manifests/foreman/server.pp
@@ -1,4 +1,5 @@
 class rbaselines::foreman::server {
   include rbaselines::foreman::server::puppet_lint  
   include rbaselines::foreman::server::vimrc
+  include rbaselines::foreman::server::column_view
 }
