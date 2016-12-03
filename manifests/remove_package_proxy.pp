b44bd25039f1d682c6891ec769027c6bdd128f50
root <root@foreman.localdomain>
2016-12-03 17:21:39 +0000

    Foreman column view plugin.

---
+++ manifests/remove_package_proxy.pp
@@ -0,0 +1,34 @@
+class rbaselines::remove_package_proxy (
+) inherits rbaselines::params {
+
+
+  Class['rbaselines::basic_packages'] -> Class['rbaselines::remove_package_proxy']
+
+  case $::osfamily {
+    'RedHat': {
+      file_line { 'remove_proxy':
+        ensure   => absent,
+        path     => '/etc/yum.conf',
+        multiple => true,
+        match    => '^proxy.*',
+        line     => 'proxy = http://foreman:3128',
+      }
+    }
+    'Debian': {
+      file { '/etc/apt/apt.conf':
+        ensure => file;
+      }
+      file_line { 'remove_proxy':
+        ensure   => absent,
+        path     => '/etc/apt/apt.conf',
+        multiple => true,
+        match    => '^Acquire::http::Proxy.*',
+        line     => 'Acquire::http::Proxy "http://foreman:3128";',
+      }
+    }
+    default: { fail("Not supported: ${::osfamily}") }
+
+  }
+
+}
+
