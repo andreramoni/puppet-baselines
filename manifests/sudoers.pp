d632cf15f1e8f7cf7217ed9ae71bbc75e7c80f08
Andre Ramoni <andre.ramoni@gmail.com>
2016-12-17 00:02:35 -0200

    .

--- manifests/sudoers.pp
+++ manifests/sudoers.pp
@@ -2,7 +2,7 @@ class rbaselines::sudoers (
   $sudo_admins_file = $::rbaselines::params::sudo_admins_file,
   $sudo_groups = $::rbaselines::params::sudo_groups,
 ) inherits rbaselines::params {
-  if $sudo_groups {
+  if $sudo_groups != '' {
     file { $sudo_admins_file:
       ensure  => 'present',
       content => template('rbaselines/sudoers/admins.erb'),
