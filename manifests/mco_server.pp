f41340bbbdd26709d0ee281e28e8ab698eb373a8
andreramoni <ramoni@Andres-iMac.local>
2017-01-20 20:59:50 -0200

    Skip debian on this class

--- manifests/mco_server.pp
+++ manifests/mco_server.pp
@@ -3,11 +3,13 @@ class rbaselines::mco_server (
   $mcoclient_password = $::rbaselines::params::mcoclient_password,
   $mco_broker = $::rbaselines::params::mco_broker,
 ) inherits rbaselines::params {
-  class { '::mcollective':
-    client              => false,
-    middleware_user     => $mcoclient_user,
-    middleware_password => $mcoclient_password,
-    middleware_hosts    => [ $mco_broker ],
+  unless $::osfamily == 'Debian' {
+    class { '::mcollective':
+      client              => false,
+      middleware_user     => $mcoclient_user,
+      middleware_password => $mcoclient_password,
+      middleware_hosts    => [ $mco_broker ],
+    }
   }
 
 }
