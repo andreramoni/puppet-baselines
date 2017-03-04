41e0e27a83defa1af3e3b4c6df524626d30678c8
Andre Ramoni <ramoni@Portatrecos.local>
2017-03-04 20:22:38 -0300

    .

--- manifests/foreman/proxy/iptables.pp
+++ manifests/foreman/proxy/iptables.pp
@@ -3,16 +3,16 @@ class rbaselines::foreman::proxy::iptables {
     chain    => 'POSTROUTING',
     jump     => 'MASQUERADE',
     proto    => 'all',
-    outiface => ${::networking.primary},
+    outiface => $facts['networking']['primary'],
     table    => 'nat',
   }
   firewall { '01 REDIRECT':
-    chain => 'PREROUTING',
-    jump  => 'REDIRECT',
-    proto => 'udp',
-    dport  => '53',
+    chain   => 'PREROUTING',
+    jump    => 'REDIRECT',
+    proto   => 'udp',
+    dport   => '53',
     toports => '53',
-    table => 'nat',
+    table   => 'nat',
   }
 
 }
