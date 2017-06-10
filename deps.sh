65dc718cb130e563df634e05da0cc1033f963bd4
root <root@foreman.ramoni.com.br>
2017-06-10 01:09:20 +0000

    .

--- deps.sh
+++ deps.sh
@@ -3,4 +3,6 @@ puppet module install saz-timezone
 puppet module install puppetlabs-firewall
 puppet module install puppet-mcollective
 puppet module install yo61-logrotate
+
 puppet module install petems-swap_file
+
