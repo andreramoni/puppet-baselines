633c51486000707882f08bdceb36c1937f40fc37
Andre Guimaraes <andreg@stone.com.br>
2017-11-25 00:12:18 -0200

    removed gpg check from puppetlabs deps repo

--- manifests/repos/puppetlabs_deps.pp
+++ manifests/repos/puppetlabs_deps.pp
@@ -4,8 +4,6 @@ class rbaselines::repos::puppetlabs_deps {
     baseurl  => 'http://yum.puppetlabs.com/el/7/dependencies/$basearch',
     descr    => 'Puppet Labs Dependencies El 7 - $basearch',
     enabled  => '1',
-    gpgcheck => '1',
-    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs
-       file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppet',
+    gpgcheck => '0',
   }
 }
