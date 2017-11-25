1b5d397b648f25da84195e0ed2431594d6865b3f
Andre Guimaraes <andreg@stone.com.br>
2017-11-25 05:16:23 -0200

    Added pubkey for foreman proxy.

---
+++ manifests/foreman_proxy_pubkey.pp
@@ -0,0 +1,10 @@
+class rbaselines::foreman_proxy_pubkey (
+  $foreman_proxy_pubkey = $::rbaselines::params::foreman_proxy_pubkey,
+) {
+  ssh_authorized_key {  'sshkey-foreman_proxy':
+    ensure => present,
+    type   => 'ssh-rsa',
+    key    => $::foreman_proxy_pubkey,
+    user   => 'root',
+  }
+}
