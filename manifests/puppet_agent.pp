f7973efa355cce818de736034cf994e267a731ec
Andre Guimaraes <andreg@stone.com.br>
2018-04-23 16:48:21 -0300

    .

--- manifests/puppet_agent.pp
+++ manifests/puppet_agent.pp
@@ -9,7 +9,7 @@ class rbaselines::puppet_agent (
   augeas { 'puppet_conf':
     context => '/files/etc/puppetlabs/puppet/puppet.conf',
     changes => [ "set agent/server ${puppet_server}",
-                 "set agent/runinterval 60", ],
+                 "set agent/runinterval ${puppet_runinterval}", ],
     notify  => Service[$puppet_agent_service],
   }
 }
