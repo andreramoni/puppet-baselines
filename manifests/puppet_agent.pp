bfaa338712723bd38072529b4566f5b3deeac89c
Andre Guimaraes <andreg@stone.com.br>
2018-04-23 16:52:08 -0300

    .

--- manifests/puppet_agent.pp
+++ manifests/puppet_agent.pp
@@ -1,6 +1,7 @@
 class rbaselines::puppet_agent (
   $puppet_agent_service = $::rbaselines::params::puppet_agent_service,
   $puppet_server = $::rbaselines::params::puppet_server,
+  $puppet_runinterval = $::rbaselines::params::puppet_runinterval,
 ) inherits rbaselines::params {
   service { $puppet_agent_service:
     ensure => running,
