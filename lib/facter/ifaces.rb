1a77422548002f0be42d244afc0859eeb08f495c
Andre Ramoni <andre.ramoni@gmail.com>
2018-04-25 21:04:29 -0300

    .

---
+++ lib/facter/ifaces.rb
@@ -0,0 +1,47 @@
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_mgmt") do
+ 		setcode 'ip a sh | grep -P "inet 10.255" | awk \'{print $NF}\' | head -n 1'
+	end
+end
+
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_mgmt_ip") do
+ 		setcode 'ip a sh | grep -P "inet 10.255" | awk \'{print $2}\' | head -n 1 | cut -f1 -d/'
+	end
+end
+
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_ext") do
+ 		setcode 'ip a sh | grep -P "inet 192.168.12\." | awk \'{print $NF}\' | grep -v lo | head -n 1'
+	end
+end
+
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_ext_ip") do
+ 		setcode 'ip a sh | grep -P "inet 192.168.12\." | awk \'{print $2}\' | head -n 1 | cut -f1 -d/'
+	end
+end
+
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_srv") do
+ 		setcode 'ip a sh | grep -P "inet 10.0\." | awk \'{print $NF}\' | head -n 1'
+	end
+end
+
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_srv_ip") do
+ 		setcode 'ip a sh | grep -P "inet 10.0\." | awk \'{print $2}\' | head -n 1 | cut -f1 -d/'
+	end
+end
+
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_dmz1") do
+ 		setcode 'ip a sh | grep -P "inet 10.1\." | awk \'{print $NF}\' | head -n 1'
+	end
+end
+
+if Facter.value('kernel') == 'Linux'
+	Facter.add("iface_dmz1_ip") do
+ 		setcode 'ip a sh | grep -P "inet 10.1\." | awk \'{print $2}\' | head -n 1 | cut -f1 -d/'
+	end
+end
