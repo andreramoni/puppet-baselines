4fce0773b0840a8d135e89c666127dd40a7c2256
Andre Ramoni <andre.ramoni@gmail.com>
2018-04-27 22:11:15 -0300

    .

--- manifests/puppet_agent.pp
+++ manifests/puppet_agent.pp
@@ -2,6 +2,8 @@ class rbaselines::puppet_agent (
   $puppet_agent_service = $::rbaselines::params::puppet_agent_service,
   $puppet_server = $::rbaselines::params::puppet_server,
   $puppet_runinterval = $::rbaselines::params::puppet_runinterval,
+  $puppet_splay = $::rbaselines::params::puppet_splay,
+  $puppet_splaylimit = $::rbaselines::params::puppet_splaylimit,
 ) inherits rbaselines::params {
   service { $puppet_agent_service:
     ensure => running,
@@ -10,7 +12,9 @@ class rbaselines::puppet_agent (
   augeas { 'puppet_conf':
     context => '/files/etc/puppetlabs/puppet/puppet.conf',
     changes => [ "set agent/server ${puppet_server}",
-                 "set agent/runinterval ${puppet_runinterval}", ],
+                 "set agent/runinterval ${puppet_runinterval}", 
+                 "set agent/splay ${puppet_splay}", 
+                 "set agent/splaylimit ${puppet_splaylimit}", ],
     notify  => Service[$puppet_agent_service],
   }
 }
