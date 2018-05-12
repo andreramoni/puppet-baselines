357e4ab7385afe82815db1e5b6650199f34ca803
Andre Ramoni <andre.ramoni@gmail.com>
2018-05-12 18:56:17 -0300

    .

--- manifests/linux/sysctl_common.pp
+++ manifests/linux/sysctl_common.pp
@@ -1,7 +1,7 @@
 class rbaselines::linux::sysctl_common (
   $sysctl_options        = $::rbaselines::params::sysctl_options,
 ) inherits rbaselines::params {
-  rbaselines::sysctl { 'common':
+  rbaselines::linux::sysctl { 'common':
     sysctl_options        => $sysctl_options,
   }
 }
