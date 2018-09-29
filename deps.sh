ed3e9f2c9a3a378bd422c3f8dbf584fdc7d0a46a
Andre Ramoni <andre.ramoni@gmail.com>
2018-09-28 22:45:33 -0300

    .

--- deps.sh
+++ deps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-puppet module install puppetlabs-ntp
-puppet module install saz-timezone
-puppet module install puppet-mcollective
+#puppet module install puppetlabs-ntp
+#puppet module install saz-timezone
+#puppet module install puppet-mcollective
 puppet module install puppetlabs-chocolatey
-puppet module install aboe-chrony
-puppet module install tse-winntp 
-git clone https://github.com/rismoney/puppet-windowsnetwork.git ../windowsnetwork
-puppet module install crayfishx-network_config
+#puppet module install aboe-chrony
+#puppet module install tse-winntp 
+#git clone https://github.com/rismoney/puppet-windowsnetwork.git ../windowsnetwork
+#puppet module install crayfishx-network_config
