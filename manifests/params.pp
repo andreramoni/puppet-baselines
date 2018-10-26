1252e4d60cd9c1409a663412dea9ed54bffdfbf2
Andre Ramoni <andre.ramoni@gmail.com>
2018-10-26 19:55:04 -0300

    .

--- manifests/params.pp
+++ manifests/params.pp
@@ -43,7 +43,7 @@ class rbaselines::params {
     'Debian' => 'ssh',
     default  => undef,
   }
-  $sshd_permitrootlogin = 'no'
+  $sshd_permitrootlogin = 'yes'
   $sshd_x11forwarding   = 'no'
   $sshd_usedns          = 'no'
   $sshd_printmotd       = 'no'
